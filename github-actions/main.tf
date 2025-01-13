d80d43d063a7c50200b204ffed6539172916d52e
Damián Silvani <munshkr@users.noreply.github.com>
2025-01-13 10:53:17 -0300

    fix(github-actions): Include `owner` variable in case repositories are from a different owner than authenticated user (#2)

--- github-actions/main.tf
+++ github-actions/main.tf
@@ -1,3 +1,7 @@
+provider "github" {
+  owner = var.owner
+}
+
 resource "google_iam_workload_identity_pool" "default" {
   project                   = var.project
   workload_identity_pool_id = "github"
@@ -42,7 +46,7 @@ resource "google_project_iam_member" "default" {
 resource "github_actions_variable" "project_id" {
   for_each = toset(var.repositories)
 
-  repository    = each.value
+  repository    = each.key
   variable_name = "GCP_PROJECT_ID"
   value         = var.project
 }
@@ -50,7 +54,7 @@ resource "github_actions_variable" "project_id" {
 resource "github_actions_variable" "workload_identity_provider" {
   for_each = toset(var.repositories)
 
-  repository    = each.value
+  repository    = each.key
   variable_name = "GCP_WORKLOAD_IDENTITY_PROVIDER"
   value         = google_iam_workload_identity_pool_provider.default.name
 }
@@ -58,7 +62,7 @@ resource "github_actions_variable" "workload_identity_provider" {
 resource "github_actions_variable" "service_account" {
   for_each = toset(var.repositories)
 
-  repository    = each.value
+  repository    = each.key
   variable_name = "GCP_SERVICE_ACCOUNT"
   value         = google_service_account.default.email
 }
