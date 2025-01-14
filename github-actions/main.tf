8f2d93eec76f1a27e75bc0a996cb88dfd7853dfc
Damián Silvani <munshkr@gmail.com>
2025-01-14 09:23:33 -0300

    fix(github-actions): Remove attribute_condition

--- github-actions/main.tf
+++ github-actions/main.tf
@@ -17,9 +17,9 @@ resource "google_iam_workload_identity_pool_provider" "default" {
     "attribute.aud"        = "assertion.aud"
     "attribute.repository" = "assertion.repository"
   }
-  attribute_condition = join(" || ", [
-    for repo in var.repositories : "attribute.repository == \"${var.owner}/${repo}\""
-  ])
+  # attribute_condition = join(" || ", [
+  #   for repo in var.repositories : "attribute.repository == \"${var.owner}/${repo}\""
+  # ])
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
